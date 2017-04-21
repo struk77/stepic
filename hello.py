1219ad5e1a0691d49d285334e5db2b5f12dc389d
struk77 <struk@yandex.ru>
2017-04-21 12:19:06 +0000

    ccc

--- hello.py
+++ hello.py
@@ -1,12 +1,12 @@
 def app(environ, start_response):
     query = environ.get('QUERY_STRING')
     queries = query.split('&')
-    body = ''
+    body = []
     for str in queries:
-        body = body + str + '\n'
+        body.append(str + '\n')
     headers = [
       ('Content-Type','text/plain')
     ]
     status = '200 OK'
     start_response(status,headers)
-    return [body]
+    return body
