a1817b6e1ac6595095cc97d8c9478584c5bbcd7a
epicwn <epicwn@aol.com>
2017-04-21 14:51:44 +0300

    bbb

--- hello.py
+++ hello.py
@@ -1,10 +1,12 @@
 def app(environ, start_response):
-   query = environ.get('QUERY_STRING')
-   
-   body = 'ok'
-   headers = [
+    query = environ.get('QUERY_STRING')
+    queries = query.split('&')
+    body = ''
+    for str in queries:
+        body = body + str + '\n'
+    headers = [
       ('Content-Type','text/plain')
-   ]
-   status = '200 OK'
-   start_response(status,headers)
-   return [body]
+    ]
+    status = '200 OK'
+    start_response(status,headers)
+    return [body]
