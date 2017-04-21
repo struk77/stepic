d3aaa36a472b7f1751bb10957057faa32af314d4
epicwn <epicwn@aol.com>
2017-04-21 15:32:43 +0300

    eee

--- init.sh
+++ init.sh
@@ -1,2 +1,4 @@
 sudo ﻿ln -s /home/box/web/etc/nginx.conf  /etc/nginx/sites-enabled/test.conf
 sudo /etc/init.d/nginx restart
+cp /home/box/web/etc/hello.py /home/box/etc/hello.py
+sudo .ln -s /home/box/etc/hello.py /etc/gunicorn.d/hello.py
