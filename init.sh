ec970ba92ea234432adc1040cc567712f6a941af
struk77 <struk@yandex.ru>
2017-04-21 13:04:03 +0000

    fff

--- init.sh
+++ init.sh
@@ -1,4 +1,5 @@
-sudo ﻿ln -s /home/box/web/etc/nginx.conf  /etc/nginx/sites-enabled/test.conf
+sudo ln -s /home/box/web/etc/nginx.conf  /etc/nginx/sites-enabled/test.conf
 sudo /etc/init.d/nginx restart
+mkdir /home/box/etc
 cp /home/box/web/etc/hello.py /home/box/etc/hello.py
-sudo .ln -s /home/box/etc/hello.py /etc/gunicorn.d/hello.py
+sudo ln -s /home/box/etc/hello.py /etc/gunicorn.d/hello.py
