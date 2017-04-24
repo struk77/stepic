66eba534a92f465c3a492720afc7d0befae0bc20
epicwn <epicwn@aol.com>
2017-04-24 16:42:04 +0300

    first django commit fixed

--- ask/qa/views.py
+++ ask/qa/views.py
@@ -4,5 +4,13 @@ from django.shortcuts import render
 
 from django.http import HttpResponse
 
+import logging
+
+logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
+					level=logging.INFO)
+
+logger = logging.getLogger(__name__)
+
 def test(request, *args, **kwargs):
+    logger.INFO("huyase")
     return HttpResponse('OK')
