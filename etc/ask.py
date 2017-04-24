009144d258ded8c64bf4be98442c9f15ef2396b7
epicwn <epicwn@aol.com>
2017-04-24 17:35:16 +0300

    first django commit fixed

--- etc/ask.py
+++ etc/ask.py
@@ -6,7 +6,7 @@ CONFIG = {
         '--bind=0.0.0.0:8000',
         '--workers=16',
         '--timeout=60',
-        '--pythonpath="/home/box/web/ask"'
+        '--pythonpath="/home/box/web/ask"',
         'ask.wsgi:application',
     ),
 }
