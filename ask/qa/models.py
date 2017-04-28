f9e747898c95db7037fb3d3145b8432815649dc6
epicwn <epicwn@aol.com>
2017-04-28 14:51:21 +0300

    3

--- ask/qa/models.py
+++ ask/qa/models.py
@@ -2,29 +2,34 @@ from django.db import models
 from django.contrib.auth.models import User
 
 # Create your models here.
+class QuestionManager(models.Manager):
+    def new(self):
+        return self.order_by('-added_at')
+    def popular(self):
+        return self.order_by('-rating')   
 
 class Question(models.Model):
+    objects = QuestionManager()
+    
     title = models.CharField(max_length=255)
     text = models.TextField()
     added_at = models.DateField(auto_now_add=True)
     rating = models.IntegerField(default=0)
-    author = models.ForeignKey(User,on_delete=models.DO_NOTHING)
+    author = models.ForeignKey(User, null=False)
     likes = models.ManyToManyField(User, related_name='question_like_user',blank=True)
+
     def __unicode__(self):
         return self.title
+    def get_url(self):
+        return '/question/' + str(self.pk) + '/'
 
-class QuestionManager(models.Manager):
-    def new(self):
-        from django.db import connection
-        cursor = connection.cursor()
-    def popular(self):
-        from django.db import connection
-        cursor = connection.cursor()    
-    
+    class Meta:
+        ordering = ('-id',)
+   
 class Answer(models.Model):
     text = models.TextField()
     added_at = models.DateField(auto_now_add=True)
     question = models.ForeignKey(Question,on_delete=models.CASCADE)
-    author = models.ForeignKey(User,on_delete=models.DO_NOTHING)
+    author = models.ForeignKey(User)
     def __unicode__(self):
         return self.text
