bc149de28c85569953f20c09f99e1ad86f9820f2
epicwn <epicwn@aol.com>
2017-04-28 13:25:08 +0300

    начало третьей задачи

--- ask/qa/models.py
+++ ask/qa/models.py
@@ -1,3 +1,44 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+
+"""
+Question - вопрос
+title - заголовок вопроса
+text - полный текст вопроса
+added_at - дата добавления вопроса
+rating - рейтинг вопроса (число)
+author - автор вопроса
+likes - список пользователей, поставивших "лайк"
+
+Answer - ответ
+text - текст ответа
+added_at - дата добавления ответа
+question - вопрос, к которому относится ответ
+author - автор ответа
+"""
+
+class Question(models.Model):
+    title = models.CharField(max_length=255)
+    text = models.TextField()
+    added_at = models.DateField(auto_now_add=True)
+    rating = models.IntegerField()
+    author = models.ForeignKey(User,on_delete=models.SET_NULL)
+    likes = models.ManyToManyField(User, related_name='question_like_user')
+    def __unicode__(self):
+        return self.title
+
+class QuestionManager(Question):
+    def new(self):
+        pass
+    def popular(self):
+        pass
+    
+class Answer(models.Model):
+    text = models.TextField()
+    added_at = models.DateField(auto_now_add=True)
+    question = ForeignKey(Question,on_delete=models.Cascade)
+    author = models.ForeignKey(User,on_delete=models.SET_NULL)
+    def __unicode__(self):
+        return self.text
--- init.sh
+++ init.sh
@@ -3,10 +3,11 @@ sudo ln -s /home/box/web/etc/nginx.conf  /etc/nginx/sites-enabled/default
 #mkdir /home/box/etc
 #cp /home/box/web/etc/hello.py /home/box/etc/hello.py
 #cp /home/box/web/etc/ask.py /home/box/etc/ask.py
-#sudo ln -s /home/box/etc/hello.py /etc/gunicorn.d/hello.py
-#sudo ln -s /home/box/etc/ask.py /etc/gunicorn.d/ask.py
+sudo ln -s /home/box/web/etc/hello.py /etc/gunicorn.d/hello.py
+sudo ln -s /home/box/web/etc/ask.py /etc/gunicorn.d/ask.py
 git config --global user.email "struk@yandex.ru"
 git config --global user.name "struk77"
 sudo /etc/init.d/nginx restart
 #sudo /etc/init.d/gunicorn restart
 sudo gunicorn -c /home/box/web/etc/ask.py ask.wsgi:application --pythonpath='/home/box/web/ask/'
+#sudo /etc/init.d/mysql start
