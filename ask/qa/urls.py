d53fb7e9cb2782f5c5b1844e91d4922bf6905d2b
epicwn <epicwn@aol.com>
2017-05-12 15:25:29 +0300

    task 2.5 fixed urls

--- ask/qa/urls.py
+++ ask/qa/urls.py
@@ -29,7 +29,7 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-    url(r'^', views.question_all, name='question_all'),
+    url(r'^$', views.question_all, name='question_all'),
     url(r'^popular/', views.question_popular_all, name='question_popular_all'),
-    url(r'^question/(?P<question_id>[0-9]+)/$', views.question_details, name='question_details'),
+    url(r'^question/(?P<question_id>[0-9]+)/', views.question_details, name='question_details'),
 ]
